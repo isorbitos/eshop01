ee8e98f9d6eb6564d9885bcfe5b149ff883c7519
DESKTOP-CFOMFKS\User <marius.pasiulevicius@gmail.com>
2021-06-07 20:45:20 +0300

    material added

---
+++ components/Layout.js
@@ -0,0 +1,75 @@
+import Head from 'next/head'
+import React from 'react'
+import {ThemeProvider, CssBaseline, AppBar, Toolbar, Link, Container, Box, Typography} from '@material-ui/core'
+import { theme, useStyles } from '../utils/styles'
+import NextLink from 'next/link';
+
+export default function Layout({
+    children,
+    commercePublicKey,
+    title = 'E-shop'
+}){
+
+        const classes = useStyles()
+        return(
+            <React.Fragment>
+                <Head>
+                    <meta charSet='utf-8' />
+                    <title>{`${title} - supper shop`}</title>
+                    <link rel='icon' href='/favicon.ico' />
+                    <meta 
+                        name="viewport"
+                        content="width=device-width , initial-scale=1, shrink-to-fit=no"
+                    />
+                </Head>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <AppBar 
+                        position='static'
+                        color='default'
+                        elevation={0}
+                        className={classes.appBar}
+                    >
+                        <Toolbar className={classes.toolbar}>
+                            <NextLink href='/'>
+                                <Link 
+                                variant="h6"
+                                color="inherit"
+                                noWrap
+                                href='/'
+                                className={classes.toolbarTitle}
+                                >
+                                    E-Shop
+                                </Link>
+                            </NextLink>
+                            <nav>
+                                <NextLink href='/cart'>
+                                    <Link 
+                                    variant="button"
+                                    color='textPrimary'
+                                    href='/cart'
+                                    className={classes.link}
+                                    >
+                                    Cart
+                                    </Link>
+
+                                </NextLink>
+                            </nav>
+                        </Toolbar>
+                    </AppBar>
+                    <Container component='main' className={classes.main}>
+                        {children}
+                    </Container>
+                    <Container maxWidth="md" component="footer">
+                        <Box mt={5}>
+                            <Typography variant="body2" color="textSecondary" align="center">
+                            {'© '}
+                            Eshop {new Date().getFullYear()}
+                            {'.'}
+                            </Typography>
+                        </Box>
+        </Container>
+                </ThemeProvider>
+            </React.Fragment>
+        )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css'
 import getCommerce from '../utils/commerce'
 
@@ -8,20 +9,20 @@ export default function Home(props) {
   const {products} = props;
 
   return (
-    <div >
- 
-      
+    <Layout title="Home" commercePublicKey={props.commercePublicKey}>
+        <div >
         {products.map((product)=>(
           <div key={product.id}>
             <img src={product.media.source} alt={product.name}/>
             <p>{product.name}</p>
             <p>{product.price.formatted_with_symbol}</p>
+            <hr></hr>
+            {product.description}
           </div>
         ))}
-      
-    
-
     </div>
+    </Layout>
+    
   )
 }
 
@@ -30,6 +31,7 @@ export async function getStaticProps(){
   const commerce = getCommerce();
   // we getting data and rename to products
   const { data: products } = await commerce.products.list();
+  
   return{
     props:{
       products,
